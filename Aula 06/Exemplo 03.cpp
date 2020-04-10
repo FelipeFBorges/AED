f8b738d45ceade03bf8796c3400e41b7e1bb946d
Felipe <61751387+FelipeFBorges@users.noreply.github.com>
2020-04-09 22:37:33 -0300

    Create Exemplo 03.cpp

---
+++ Aula 06/Exemplo 03.cpp
@@ -0,0 +1,33 @@
+
+# include  < pilha >
+
+usando  espaço para nome  std ;
+
+void  imprime_pilha (pilha < int > pilha)
+{
+	printf ( " Pilha: \ n " );
+	
+	while (! pilha. empty ())
+	{
+		printf ( " % d \ n " , pilha. top ());
+		
+		pilha. pop ();
+	}
+	printf ( " \ n " );
+}
+
+int  main ()
+{	
+	pilha < int > pilha;
+	
+	pilha. empurrão ( 10 );
+	pilha. empurrão ( 9 );
+	pilha. empurrão ( 8 );
+	pilha. empurrão ( 7 );
+	
+	imprime_pilha (pilha);
+	pilha. pop ();
+	imprime_pilha (pilha);
+	pilha. pop ();
+	imprime_pilha (pilha);
+}
